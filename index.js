a93b2e67040f30a9c75447a5f50aef8be5508bca
ColeHuntley83 <cole@trackonomysystems.com>
2022-12-28 15:09:27 -0800

    port change

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express()
-
+const port = process.env.PORT || 4000;
 const env = process.env.ENVIRONMENT
 app.get("/ping", (req, res) => {
+    console.log(process.env)
     res.send("test2")
 })
 
 
-app.listen(3001, () => {
-    console.log("APP is listening on Port: ", 3001)
+app.listen(port, () => {
+    console.log("APP is listening on Port: ", port)
 })
