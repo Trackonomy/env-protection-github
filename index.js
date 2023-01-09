86b29abed6b8f69124bb397db89d9656d802bbae
ColeHuntley83 <cole@trackonomysystems.com>
2023-01-09 10:56:10 -0800

    chnaged port env

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express()
 
 const env = process.env.ENVIRONMENT
+const PORT = parseInt(process.env.PORT || 8080)
 app.get("/ping", (req, res) => {
     res.send("test")
 })
 
 
-app.listen(3001, () => {
-    console.log("APP is listening on Port: ", 3001)
+app.listen(PORT, () => {
+    console.log("APP is listening on Port: ", PORT)
 });
